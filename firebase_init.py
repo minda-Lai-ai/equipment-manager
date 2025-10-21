923aaedfd6c1d9a5f2295e5a788330de536060c4
minda-Lai-ai <laimindar@gmail.com>
2025-10-21 13:14:22 +0800

    Update firebase_init.py

--- firebase_init.py
+++ firebase_init.py
@@ -3,12 +3,12 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import streamlit as st
 
+APP_NAME = "equipment_manager_app"
+
 def get_firestore():
     try:
-        # 嘗試取得已初始化的 Firebase App
-        app = firebase_admin.get_app()
+        app = firebase_admin.get_app(APP_NAME)
     except ValueError:
-        # 尚未初始化 → 執行初始化
         cred = credentials.Certificate(eval(st.secrets["firebase_adminsdk"]))
-        app = firebase_admin.initialize_app(cred)
+        app = firebase_admin.initialize_app(cred, name=APP_NAME)
     return firestore.client(app)
