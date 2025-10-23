8c668ae8bf64ce110435aef0dd5b2f0560b4cd0e
minda-Lai-ai <laimindar@gmail.com>
2025-10-23 22:25:22 +0800

    Update equipment_detail.py

--- pages/equipment_detail.py
+++ pages/equipment_detail.py
@@ -69,9 +69,7 @@ for col in row.index:
 st.markdown("---")
 
 # 編輯按鈕（正確跳分頁）
-st.download_button(
 st.page_link("pages/edit_data.py", label="✏️ 編輯此設備 ✏️✏️", icon="✏️")
-)
     
 # 儲存為 CSV 檔供下載
 csv_data_bom = '\ufeff' + row_df.to_csv(index=False)
