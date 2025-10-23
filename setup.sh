5f59802bd788ef98715f818399b7ea659c68dd8d
minda-Lai-ai <laimindar@gmail.com>
2025-10-23 22:04:34 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 安裝中文字型
+apt-get update
+apt-get install -y fonts-arphic-ukai fonts-arphic-uming
+# 或微軟正黑體： apt-get install -y fonts-msjh
+# 或安裝 SimHei: apt-get install -y fonts-wqy-zenhei fonts-wqy-microhei
